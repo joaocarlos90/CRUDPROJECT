5097140944054556572f46bb0c015f7189aaab7d
Joao Carlos Alves <suporte_carlos@live.com>
2020-03-08 20:51:59 -0300

    first commit on project update and insert

---
+++ SistemaWebControleEstoque/App_Code/ProdutoBLL.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+
+/// <summary>
+/// Descrição resumida de ProdutoBLL
+/// </summary>
+public class ProdutoBLL
+{
+    public string Nome { get; set; }
+    public string Descricao { get; set; }
+    public decimal Preco_Custo { get; set; }
+    public decimal Preco_Venda { get; set; }
+    public double Quantidade { get; set; }
+    public string Unidade_Medida { get; set; }
+    public int Categoria_Id { get; set; }
+
+    DAL objDAL = new DAL();
+
+    public DataTable RetCategoriasProdutos()
+    {
+        return objDAL.RetDataTable("select*from categoria");
+    } 
+
+    public DataTable RetListarProdutos()
+    {
+        return objDAL.RetDataTable("select*from produto");
+    }
+    //pesquisar por ID
+
+    public DataTable CarregarProdutoPorId(string id)
+    {
+        return objDAL.RetDataTable("select*from produto where id= "+id);
+    }
+
+
+
+
+    public void InserirProduto()
+    {
+        string sql = "INSERT INTO produto(nome, descricao, preco_custo, preco_venda, quantidade, unidade_medida, categoria_id)" +
+                        "VALUES('{0}','{1}','{2}','{3}','{4}','{5}','{6}')";
+
+        sql = string.Format(sql, Nome, Descricao, Preco_Custo, Preco_Venda, Quantidade, Unidade_Medida, Categoria_Id);
+        objDAL.ExecutarComandoSQL(sql);
+    }
+    
+    public void AlterarProduto(string id)
+    {
+        string sql = "UPDATE produto SET nome='{0}', descricao = '{1}', preco_custo='{2}', preco_venda='{3}', quantidade='{4}', unidade_medida='{5}', categoria_id='{6}' WHERE id='{7}'";
+        sql = string.Format(sql, Nome, Descricao, Preco_Custo, Preco_Venda, Quantidade, Unidade_Medida, Categoria_Id, id);
+
+        objDAL.ExecutarComandoSQL(sql);
+    }
+}
---
+++ SistemaWebControleEstoque/Produto.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Produto : System.Web.UI.Page
+{
+    ProdutoBLL objProduto = new ProdutoBLL();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        CarregarCategorias();
+        CarregarGridView();
+    }
+
+    public void CarregarCategorias()
+    {
+        ddlcategoriaid.DataSource = objProduto.RetCategoriasProdutos();
+        ddlcategoriaid.DataValueField = "id";
+        ddlcategoriaid.DataTextField = "nome";
+        ddlcategoriaid.DataBind();
+    }
+
+    private void CarregarGridView()
+    {
+        gdproduto.DataSource = objProduto.RetListarProdutos();
+        gdproduto.DataBind();
+    }
+
+    protected void btngravar_Click(object sender, EventArgs e)
+    {
+        objProduto.Nome = txtnome.Text;
+        objProduto.Descricao = txtdescricao.Text;
+        objProduto.Preco_Custo = decimal.Parse(txtprecocusto.Text);
+        objProduto.Preco_Venda = decimal.Parse(txtprecovenda.Text);
+        objProduto.Quantidade = double.Parse(txtquantidade.Text);
+        objProduto.Unidade_Medida = txtunidademedida.Text;
+        objProduto.Categoria_Id = int.Parse(ddlcategoriaid.SelectedValue.ToString());
+
+        if (string.IsNullOrEmpty(txtid.Text))
+        {
+            objProduto.InserirProduto();
+        }
+        else
+        {
+            objProduto.AlterarProduto(txtid.Text);
+        }
+
+        CarregarGridView();
+    }
+
+
+
+    protected void btncarregar_Click(object sender, EventArgs e)
+    {
+        DataTable data = objProduto.CarregarProdutoPorId(txtid.Text);
+
+        txtnome.Text = data.Rows[0]["nome"].ToString();
+        txtdescricao.Text = data.Rows[0]["descricao"].ToString();
+        txtprecocusto.Text = data.Rows[0]["preco_custo"].ToString();
+        txtprecovenda.Text = data.Rows[0]["preco_venda"].ToString();
+        txtquantidade.Text = data.Rows[0]["quantidade"].ToString();
+        txtunidademedida.Text = data.Rows[0]["unidade_medida"].ToString();
+        ddlcategoriaid.SelectedValue = data.Rows[0]["categoria_id"].ToString();
+    }
+}
